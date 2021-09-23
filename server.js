2a25c6156d59f7906387ccb1f0ad238539aba183
Ryan <45304409+PsychedelicMonkey@users.noreply.github.com>
2021-09-22 18:34:11 -0600

    MongoDB Connection

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const dotenv = require('dotenv');
 
+dotenv.config();
 const app = express();
+
+// MongoDB Configuration
+require('./config/db');
 
 const PORT = process.env.PORT || 5000;
 
